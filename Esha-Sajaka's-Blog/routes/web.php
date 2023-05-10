7e682351fe7a9b3bf6fdbd4f6dc02a9adffe822e
Esha Sajaka <eshaapriafitrasajaka@gmail.com>
2023-05-10 22:14:33 +0700

    Create login form and registration form

--- Esha-Sajaka's-Blog/routes/web.php
+++ Esha-Sajaka's-Blog/routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
-use App\Models\Post;
-use App\Models\Category;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -42,3 +41,7 @@ Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('/categories', [CategoryController::class, 'index']);
+
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/register', [RegisterController::class, 'index']);
